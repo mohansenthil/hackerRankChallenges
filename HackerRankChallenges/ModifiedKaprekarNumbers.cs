15d9fd956184bbf11da8337201946c90386a88a7
mohansenthil <mohansenthil92@gmail.com>
2018-08-15 23:05:39 +0530

    ModifiedKaprekarNumbers
    solved ModifiedKaprekarNumbers

---
+++ HackerRankChallenges/ModifiedKaprekarNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace HackerRankChallenges
+{
+    public class ModifiedKaprekarNumbers
+    {
+
+        public static void KaprekarNumbers(int startRange, int endRange)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = startRange; i <= endRange; i++)
+            {
+                ulong squareRootOfWholeNumber = (ulong)(i * i);
+
+                var squareRootOfWholeNumberAsText = squareRootOfWholeNumber.ToString();
+
+                uint addtionOfTwoNumbers = 0;
+
+                if (squareRootOfWholeNumberAsText.Length > 1)
+                {
+                    var firstPart = squareRootOfWholeNumberAsText.Substring(0, squareRootOfWholeNumberAsText.Length / 2);
+                    var secondPart = squareRootOfWholeNumberAsText.Substring((squareRootOfWholeNumberAsText.Length / 2));
+
+                    addtionOfTwoNumbers = Convert.ToUInt32(firstPart) + Convert.ToUInt32(secondPart);
+
+                }
+
+                if (addtionOfTwoNumbers == i)
+                    builder.Append(i + " ");
+
+            }
+
+            if (builder.Length > 0)
+            {
+                Console.WriteLine(builder);
+            }
+            else
+            {
+                Console.WriteLine("INVALID RANGE");
+            }
+
+        }
+    }
+}
---
+++ HackerRankChallenges/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HackerRankChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModifiedKaprekarNumbers.KaprekarNumbers(1, 999999);
+            Console.ReadLine();
+        }
+    }
+}
