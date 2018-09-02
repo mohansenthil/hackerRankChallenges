5c6cd9c3f3c7d9f98cbf70c5f950b887e777a2a2
mohansenthil <mohansenthil92@gmail.com>
2018-09-02 22:28:46 +0530

    solved chocolate-feast
    solved chocolate-feast
    
    https://www.hackerrank.com/challenges/chocolate-feast/problem

---
+++ HackerRankChallenges/ChocolateFeast .cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRankChallenges
+{
+    public class ChocolateFeast
+    {
+         
+
+        public static int ChocolatesConsumed(int totalMoney, int chocolateCost, int wrappers)
+        {
+            int chocoaltesBought = totalMoney / chocolateCost;
+
+            int remainingWrappers = chocoaltesBought;
+
+            while (remainingWrappers >= wrappers)
+            {
+                int chocaltesBoughtWithRemainingWrappers = (remainingWrappers / wrappers);                
+
+                remainingWrappers = (remainingWrappers % wrappers) + chocaltesBoughtWithRemainingWrappers;
+
+                chocoaltesBought = chocoaltesBought + chocaltesBoughtWithRemainingWrappers ;
+            }
+
+            return chocoaltesBought;
+
+
+        }
+    }
+}
